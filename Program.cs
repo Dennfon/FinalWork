009d7348092916c99240397ff0d211f8a6ef210b
Denis <deni-alfimo@yandex.ru>
2023-07-25 18:27:23 +0300

    The scheme of the algorithm for working with arrays has been added. Added more verification arrays

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿string[] array1 = { "Hello", "2", "world", ":-)" };
-string[] array2 = new string[array1.Length];
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+
+string[] arrayWorking = new string[array1.Length];
+
 
