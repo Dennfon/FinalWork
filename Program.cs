359c56a9aab614eb5815bc4ea73c85cb814d01bd
Denis <deni-alfimo@yandex.ru>
2023-07-25 17:52:25 +0300

    2 arrays have been created, one source, the second working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = { "Hello", "2", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+
